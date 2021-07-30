b5602be0775124d5e151776dba8426bf8db8f3bc
Oscar Nieves <56237238+oscarnieves100@users.noreply.github.com>
2021-07-30 21:57:44 +1000

    Arbitrary random number sampler
    Samples random numbers out of an arbitrary probability distribution defined by the user

---
+++ Arbitrary_Random_Numbers.py
@@ -0,0 +1,52 @@
+# Arbitrary probability distribution
+#
+# Generates random numbers according to an arbitrary probability distribution 
+# p(x) defined by the user on a domain [a,b]
+#
+# Author: Oscar A. Nieves
+# Last updated: July 30, 2021
+
+import matplotlib.pyplot as plt
+import numpy as np 
+import math as mt
+plt.close('all')
+#np.random.seed(0) # Set seed
+
+# Inputs
+a = 0
+b = 5
+x0 = 2.5
+samples = 1000
+X = np.linspace(a,b,1001)
+
+# Define p(x)
+def p(x_var):
+    return (x_var - x0)**2
+P0 = p(X)
+
+# Normalize p(x)
+Psum = sum(P0)
+P = P0/Psum
+
+# Cumulative sum p(x)
+C = np.cumsum(P)
+
+# Compute numbers
+R = np.linspace(a,b,samples)
+for n in range(samples):
+    index0 = mt.ceil( min(X) + sum(sum(C[-1]*np.random.rand(1,1) > C)))
+    R[n] = X[index0]
+    
+# Generate histogram
+bins = 50
+plt.subplot(1,2,1)
+plt.plot(X,P,color='black')
+plt.xlim([a,b])
+plt.xlabel('X', fontsize=16)
+plt.ylabel('p(x)', fontsize=16)
+
+plt.subplot(1,2,2)
+plt.hist(R,bins)
+plt.xlim([a,b])
+plt.xlabel('R', fontsize=16)
+plt.ylabel('frequency', fontsize=16)
